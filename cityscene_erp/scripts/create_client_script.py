f84185316cb8b9061f14b1259cc3b8d662fae695
CityScene Global Pvt. Ltd. <citysceneglobalpvt.ltd.@CityScenes-MacBook-Air.local>
2026-07-01 11:56:14 +0530

    Add Payment Entry date automation script

---
+++ cityscene_erp/scripts/create_client_script.py
@@ -0,0 +1,28 @@
+import frappe
+
+def run():
+    frappe.init(site="cityscene-srb-erp")
+    frappe.connect()
+    
+    if not frappe.db.exists("Client Script", "Payment Entry Date Automation"):
+        doc = frappe.get_doc({
+            "doctype": "Client Script",
+            "dt": "Payment Entry",
+            "name": "Payment Entry Date Automation",
+            "module": "Accounts",
+            "script": """
+frappe.ui.form.on('Payment Entry', {
+    posting_date: function(frm) {
+        if (frm.doc.posting_date) {
+            frm.set_value('reference_date', frm.doc.posting_date);
+        }
+    }
+});
+            """,
+            "enabled": 1
+        })
+        doc.insert()
+        frappe.db.commit()
+        print("Client Script created successfully.")
+    else:
+        print("Client Script already exists.")
