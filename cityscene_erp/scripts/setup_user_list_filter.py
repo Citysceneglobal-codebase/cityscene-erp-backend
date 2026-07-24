3c4692510ab405c7921d21d939cd8d014ace0219
CityScene Global Pvt. Ltd. <citysceneglobalpvt.ltd.@CityScenes-MacBook-Air.local>
2026-07-24 05:48:54 +0530

    fix: Update script to upsert Client Script and check specific user_type filter

--- cityscene_erp/scripts/setup_user_list_filter.py
+++ cityscene_erp/scripts/setup_user_list_filter.py
@@ -1,20 +1,22 @@
 import frappe
 
 def execute():
-    # Insert Client Script for User List to hide Website Users by default
-    if not frappe.db.exists("Client Script", "Filter System Users Default"):
-        doc = frappe.get_doc({
-            "doctype": "Client Script",
-            "name": "Filter System Users Default",
-            "dt": "User",
-            "view": "List",
-            "module": "Cityscene Erp",
-            "script": """
+    # Insert or Update Client Script for User List to hide Website Users by default
+    script_content = """
 frappe.listview_settings['User'] = {
     onload: function(listview) {
-        // If there are no filters currently applied, default to System Users
+        // Check if there is already a filter for user_type
         let existing_filters = listview.filter_area.get();
-        if (existing_filters.length === 0) {
+        let has_user_type_filter = false;
+        
+        for (let i = 0; i < existing_filters.length; i++) {
+            if (existing_filters[i][1] === 'user_type') {
+                has_user_type_filter = true;
+                break;
+            }
+        }
+        
+        if (!has_user_type_filter) {
             listview.filter_area.add([
                 ['User', 'user_type', '=', 'System User']
             ]);
@@ -22,11 +24,22 @@ frappe.listview_settings['User'] = {
     }
 };
 """
+    if frappe.db.exists("Client Script", "Filter System Users Default"):
+        doc = frappe.get_doc("Client Script", "Filter System Users Default")
+        doc.script = script_content
+        doc.save(ignore_permissions=True)
+        print("Updated Client Script 'Filter System Users Default'.")
+    else:
+        doc = frappe.get_doc({
+            "doctype": "Client Script",
+            "name": "Filter System Users Default",
+            "dt": "User",
+            "view": "List",
+            "module": "Cityscene Erp",
+            "script": script_content
         })
         doc.insert(ignore_permissions=True)
         print("Created Client Script to filter User list by default.")
-    else:
-        print("Client Script 'Filter System Users Default' already exists.")
         
     frappe.db.commit()
 
